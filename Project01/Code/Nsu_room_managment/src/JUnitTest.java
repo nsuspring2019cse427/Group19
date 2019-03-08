ae4e9c417286bada79e64c516ed9b93212cbf198
farhan_israk_yen <hydro.toluyn@gmail.com>
2019-03-08 22:08:12 +0600

    login testing

--- Project01/Code/Nsu_room_managment/src/JUnitTest.java
+++ Project01/Code/Nsu_room_managment/src/JUnitTest.java
@@ -6,6 +6,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+
+
 public class JUnitTest {
 
 	@BeforeClass
@@ -18,15 +20,51 @@ public class JUnitTest {
 
 	@Before
 	public void setUp() throws Exception {
+	
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-
+// test caser
+	//1.(username=null and password=admin)
+	//2.(username=admin and password=null)
+	//3.(username=null and password=null)
+	//4.(username=admin and password=wrong)
+	//5.(username=wrong and password=admin)
+	//6.(username=wrong and password=wrong)
+	//7.(username=null and password=wrong)
+	//8.(username=wrong and password=null)
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		LoginBox log = new LoginBox();
+		if(log.userField.equals(null) )
+		{
+			if (log.equals(log.passField.equals("admin"))) {
+				System.out.println("Test1 executed");
+			}
+		}
+		
+		
+		if(log.userField.equals("admin") )
+		{
+			if (log.equals(log.passField.equals(null))) {
+				System.out.println("Test2 executed");
+			}
+		}
+		
+		
+		if(log.userField.equals(null) )
+		{
+			if (log.equals(log.passField.equals(null))) {
+				System.out.println("Test3 executed");
+			}
+		}
+		
+		
 	}
 
 }

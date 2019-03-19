1686d99930a96c175599b70f9d23bd36931fdb64
farhan_israk_yen <hydro.toluyn@gmail.com>
2019-03-20 04:07:31 +0600

    Transaction class has been tested and modified

--- Project01/Code/BankManagementSys/test/TransactionTest.java
+++ Project01/Code/BankManagementSys/test/TransactionTest.java
@@ -19,6 +19,12 @@ public class TransactionTest {
     
     int expResult = 0;
     double expResDbl = 0.0;
+    int transactionId = 0;
+    int accountNumber = 0;
+    int accountantId = 0;
+    double previousBalance = 0.0;
+    boolean isWithdraw = false;
+    double transactionBalance = 0.0;
     private Transaction trans;
     
     public TransactionTest() {
@@ -121,6 +127,184 @@ public class TransactionTest {
         } catch (Exception e) {
             fail("Problem here in this method");
         }
+    }
+
+    @Test
+    public void testSetTransactionId() {
+        try {
+            trans.setTransactionId(transactionId);
+            System.out.println("setTransactionId works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in setTransactionId method");
+        }
+    }
+
+    @Test
+    public void testSetAccountNumber() 
+    {
+        try {
+            trans.setAccountNumber(accountNumber);
+            System.out.println("setAccountNumber works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in setAccountNumber method");
+        }
+    }
+
+    @Test
+    public void testSetAccountantId() {
+        try {
+            trans.setAccountantId(accountantId);
+            System.out.println("setAccountantId works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in setAccountantId method");
+        }
+    }
+
+    @Test
+    public void testSetPreviousBalance() {
+        try {
+            trans.setPreviousBalance(previousBalance);
+            System.out.println("setPreviousBalance works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in setPreviousBalance method");
+        }
+    }
+
+    @Test
+    public void testSetIsWithdraw() {
+        try {
+            trans.setIsWithdraw(isWithdraw);
+            System.out.println("setIsWithdraw works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in setIsWithdraw method");
+        }
+    }
+
+    @Test
+    public void testSetTransactionBalance() {
+        try {
+            trans.setTransactionBalance(transactionBalance);
+            System.out.println("setTransactionBalance works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in setTransactionBalance method");
+        }
+    }
+
+    @Test
+    public void testInputIsWithdraw() {
+        try {
+            trans.inputIsWithdraw();
+            System.out.println("inputIsWithdraw works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in inputIsWithdraw method");
+        }
+    }
+
+    @Test
+    public void testInputPreviousBalance() {
+        try {
+            trans.inputPreviousBalance();
+            System.out.println("inputPreviousBalance works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in inputPreviousBalance method");
+        }
+    }
+
+    @Test
+    public void testInputTransactionBalance() {
+        try {
+            trans.inputTransactionBalance();
+            System.out.println("inputTransactionBalance works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in inputTransactionBalance method");
+        }
+    }
+
+    @Test
+    public void testInputAccountNumber() {
+        try {
+            trans.inputAccountNumber();
+            System.out.println("inputAccountNumber works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in inputAccountNumber method");
+        }
+        
+    }
+
+    @Test
+    public void testInputAccountantId() {
+        try {
+            trans.inputAccountantId();
+            System.out.println("inputAccountantId works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in inputAccountantId method");
+        }
+    }
+
+    @Test
+    public void testPrintTransactionId() {
+         try {
+            trans.printTransactionId();
+            System.out.println("printTransactionId works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in printTransactionId method");
+        }
+        
+    }
+
+    @Test
+    public void testPrintIsWithdraw() {
+        try {
+            trans.printIsWithdraw();
+            System.out.println("printIsWithdraw works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in printIsWithdraw method");
+        }
+        
+    }
+
+    @Test
+    public void testPrintPreviousBalance() {
+        try {
+            trans.printPreviousBalance();
+            System.out.println("printPreviousBalance works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in printPreviousBalance method");
+        }
+        
+    }
+
+    @Test
+    public void testPrintTransactionBalance() {
+        try {
+            trans.printTransactionBalance();
+            System.out.println("printTransactionBalance works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in printTransactionBalance method");
+        }
+       
+    }
+
+    @Test
+    public void testPrintAccountNumber() {
+        try {
+            trans.printAccountNumber();
+            System.out.println("printAccountNumber works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in printAccountNumber method");
+        }
+        
+    }
+
+    @Test
+    public void testPrintAccountantId() {
+        try {
+            trans.printAccountantId();
+            System.out.println("printAccountantId works in Transaction class");
+        } catch (Exception e) {
+            fail("Problem here in testPrintAccountantId method");
+        }
+        
     }
 
 }

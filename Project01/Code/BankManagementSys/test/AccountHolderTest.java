bd835a89c863bfa4215b476b70f095e13adcef56
Farhan1994 <farhan.yen@northsouth.edu>
2019-04-26 02:53:30 +0600

    AccountHolder class has been tested again using Mockito

--- Project01/Code/BankManagementSys/test/AccountHolderTest.java
+++ Project01/Code/BankManagementSys/test/AccountHolderTest.java
@@ -4,16 +4,25 @@
  * and open the template in the editor.
  */
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Assert;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
 
 /**
  *
@@ -101,6 +110,82 @@ public class AccountHolderTest {
         } catch (Exception e) {
             fail("The test case is a prototype.");
         }
+    }
+    
+    
+    //mockito
+    @Test
+    public void mockitoTestAccBalance()
+    {
+        try {
+            acc.setAccountBalance(1000.0);           
+            
+                when(acc.getAccountBalance()).thenReturn(1000.0);    
+               
+        } catch (Exception e) {
+            fail("Problem here");
+        }               
+    }
+    
+    @Test
+    public void mockitoTestAccBalance1()
+    {
+        try {
+            acc.setAccountBalance(-1000.0);           
+            
+                when(acc.getAccountBalance()).thenReturn(0.0);               
+               
+        } catch (Exception e) {
+            fail("Problem here");
+        }               
+    }
+    
+    @Test
+    public void mockitoTestAccBalance2()
+    {
+        try {
+            acc.setAccountBalance(0);           
+            
+                when(acc.getAccountBalance()).thenReturn(0.0);               
+               
+        } catch (Exception e) {
+            fail("Problem here");
+        }               
+    }
+    
+    @Test
+    public void mockitoTestAccNumber()
+    {
+        try {
+            acc.setAccountNumber(1);           
+            
+                when(acc.getAccountNumber()).thenReturn(1);               
+               
+        } catch (Exception e) {
+        }               
+    }
+    
+    @Test
+    public void mockitoTestAccNumber1()
+    {
+        try {
+            acc.setAccountNumber(-1);           
+            
+                when(acc.getAccountNumber()).thenReturn(0);               
+               
+        } catch (Exception e) {
+        }               
+    }
+    @Test
+    public void mockitoTestAccNumber2()
+    {
+        try {
+            acc.setAccountNumber(0);           
+            
+                when(acc.getAccountNumber()).thenReturn(0);               
+               
+        } catch (Exception e) {
+        }               
     }
     
 }
